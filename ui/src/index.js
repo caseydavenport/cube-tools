d016151adb7c4810fffad10516a48fc30a79b05b
Casey Davenport <davenport.cas@gmail.com>
2023-10-24 09:07:23 -0700

    Add webpack and babel configuration

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,14 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./styles.css";
-
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+function component() {
+  const element = document.createElement('div');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  ReactDOM.render(<App />, element)
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ ui/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "index_bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+         { loader: 'style-loader' },
+         { loader: 'css-loader' },
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
