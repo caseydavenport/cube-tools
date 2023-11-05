bb655b6a979f02c3cefd75699e180ceb621bc3d2
Casey Davenport <davenport.cas@gmail.com>
2023-11-05 15:00:24 -0800

    Add wins by # colors

--- ui/src/pages/Decks.js
+++ ui/src/pages/Decks.js
@@ -54,6 +54,15 @@ export function DeckWidget(input) {
           <WinsByOracleText decks={input.decks} />
         </td>
       </tr>
+
+      <tr style={{"height": "300px"}}>
+        <td style={{"vertical-align": "top", "width": "50%"}}>
+          <WinsByNumberOfColors decks={input.decks} />
+        </td>
+        <td style={{"vertical-align": "top"}}>
+        </td>
+      </tr>
+
     </table>
 
   )
@@ -333,6 +342,84 @@ function WinsByOracleText(input) {
       title: {
         display: true,
         text: "Wins by # removal spells",
+        color: "#FFF",
+        font: {
+          size: "16pt",
+        },
+      },
+      legend: {
+        labels: {
+          color: "#FFF",
+          font: {
+            size: "16pt",
+          },
+        },
+      },
+    },
+  };
+
+  const labels = [...wins.keys()].sort(function(a, b) {return a - b})
+  let winsData= []
+  for (let l of labels) {
+    winsData.push(wins.get(l))
+  }
+  let lossData = []
+  for (let l of labels) {
+    lossData.push(losses.get(l))
+  }
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Wins',
+        data: winsData,
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Losses',
+        data: lossData,
+        backgroundColor: '#Fa7',
+      },
+
+    ],
+  };
+
+  return (
+    <div style={{"height":"500px", "width":"100%"}}>
+      <Bar height={"300px"} width={"300px"} options={options} data={data} />;
+    </div>
+  );
+}
+
+function WinsByNumberOfColors(input) {
+  let wins = new Map()
+  let losses = new Map()
+  for (let deck of input.decks) {
+    let num = deck.colors.length
+    if (num == 0) {
+      console.log(deck)
+    }
+
+    if (!wins.has(num)) {
+      wins.set(num, 0)
+      losses.set(num, 0)
+    }
+    wins.set(num, wins.get(num) + Wins(deck))
+    losses.set(num, losses.get(num) + Losses(deck))
+  }
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {stacked:true, min: 0},
+      x: {stacked:true},
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: "Wins by # colors",
         color: "#FFF",
         font: {
           size: "16pt",
